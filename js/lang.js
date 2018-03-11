f37749332e842c5217b2fd18a88538a97085fae7
Nelson Moller <nmoller.c@gmail.com>
2018-03-11 10:28:23 -0400

    Test ...

--- js/lang.js
+++ js/lang.js
@@ -1,13 +1,23 @@
+var uqam = {
+  pluralTemps: function(temps){
+    if (temps <= 1) {
+      return "{{time}} minute";
+    }
+    return "{{time}} minutes";
+  }
+};
+
 i18next.init({
+  
   lng: 'fr',
   debug: false,
   resources: {
     fr: {
       translation: {
         "pageTitle": "Test vitesse au clavier",
-        "1minute": "1 minute",
-        "3minute": "3 minute",
-        "5minute": "5 minute",
+        "1minute": uqam.pluralTemps(1),
+        "3minute": uqam.pluralTemps(3),
+        "5minute": uqam.pluralTemps(5),
         "duration": '<i class="tiny material-icons">schedule</i> Temps du test',
         "testStart": "Le test commence dès que vous frappez la premier touche",
         "temp": "Temps : {{tmin}} minutes",
@@ -30,9 +40,9 @@ i18next.init({
 
 function updateContent() {
   document.getElementById('i18nTitle').innerHTML = i18next.t('pageTitle');
-  document.getElementById('i18n1minute').innerHTML = i18next.t('1minute');
-  document.getElementById('i18n3minute').innerHTML = i18next.t('3minute');
-  document.getElementById('i18n5minute').innerHTML = i18next.t('5minute');
+  document.getElementById('i18n1minute').innerHTML = i18next.t('1minute', {time: 1});
+  document.getElementById('i18n3minute').innerHTML = i18next.t('3minute', {time: 3});
+  document.getElementById('i18n5minute').innerHTML = i18next.t('5minute', {time: 5});
   document.getElementById('i18nDuration').innerHTML = i18next.t('duration');
   document.getElementById('typedTextBox').placeholder = i18next.t('testStart');
 }
